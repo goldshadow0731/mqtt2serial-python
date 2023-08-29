7b9378277258f4c1fc45902fae4cab46df8bde7b
shadow <davidfang148@gmail.com>
2023-08-29 15:35:40 +0800

    add control

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # -*- coding: UTF-8 -*-
 import os
 
-from paho.mqtt.client import Client
+from paho.mqtt.client import Client, MQTTMessage
 import serial
 
 
 client = Client()
+port = serial.Serial(os.getenv("SERIAL_PORT"),
+                     int(os.getenv("SERIAL_BAUDRATE")))
 
 
 @client.connect_callback()
 def on_connect(client: Client, userdata, flags, result):
     print(f"=============== {'Connected':^15s} ===============")
+    if os.getenv("MQTT_RECEIVE_TOPIC", None):
+        client.subscribe(os.getenv("MQTT_RECEIVE_TOPIC"))
 
 
 @client.disconnect_callback()
@@ -18,12 +22,26 @@ def on_disconnect(client: Client, userdata, result):
     print(f"=============== {'Disconnected':^15s} ===============")
 
 
+def on_message(client: Client, userdata, msg: MQTTMessage):
+    if port.is_open:
+        port.write(msg.payload)
+        port.write(b"\r\n")
+
+
+client.on_message = on_message if os.getenv("MQTT_RECEIVE_TOPIC") else None
+
+
 if __name__ == "__main__":
     try:
         client.connect(os.getenv("MQTT_BROKER"), int(os.getenv("MQTT_PORT")))
+        client.loop_start()
+        if not port.is_open:
+            port.open()
+        if not port.is_open:
+            raise serial.SerialException("Serial port open failed.")
         while True:
-            with serial.Serial(os.getenv("SERIAL_PORT"), int(os.getenv("SERIAL_BAUDRATE"))) as port:
-                client.publish(os.getenv("MQTT_TOPIC"),
-                               port.readline().decode().rstrip("\r\n"))
+            client.publish(os.getenv("MQTT_SEND_TOPIC"),
+                           port.readline().decode().rstrip("\r\n"))
     except:
-        pass
+        port.close()
+        client.loop_stop()
